f635d580e6034aaf3f1e1f6be23d49a01ccf2f92
Damian Kluziak <dkluziak303@gmail.com>
2017-10-16 15:23:05 +0100

    Created several methods to split the tasks

--- src/Crawler/Crawler.java
+++ src/Crawler/Crawler.java
@@ -1,6 +1,7 @@
 package Crawler;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,20 +10,59 @@ import org.jsoup.select.Elements;
 
 
 public class Crawler {
+	
+	private static ArrayList<String> linkList = new ArrayList<String>();
 
 	public static void main(String[] args) throws IOException {
-        String url = "https://icpe2017.spec.org/icpe2017.html";
-        print("Fetching %s...", url);
 
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("ul a[href]");
-
-        print("\nLinks: (%d)", links.size());
-        for (Element link : links) {
-            print(" * a: <%s>", link.attr("abs:href"));
+		Crawler test = new Crawler();
+		test.getAllLinks();
+        print("\nLinks: (%d)", linkList.size());
+        int i = 1;
+        for (String link : linkList) {
+            print(i + " * a: <%s>", link);
+            i++;
         }
     }
+	
+	private void getAllLinks() {
+        String url = "https://icpe2017.spec.org/icpe2017.html";
+        print("Fetching %s...", url);
+        Document doc = null;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Elements links = doc.select("ul a[href]");
+        this.addToLinkList(links);
+        this.getSublinks(links);
+        
+	}
 
+	private void getSublinks(Elements links) {
+		Document doc = null;
+		Elements sublinks = null;
+		for(Element link: links) {
+			try {
+				doc = Jsoup.connect(link.attr("abs:href")).ignoreContentType(true).get();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+			sublinks = doc.select("ul li ul a[href]");
+			this.addToLinkList(sublinks);
+		}	
+	}
+	
+	private void addToLinkList(Elements links) {
+		for(Element link: links) {
+			linkList.add(link.attr("abs:href"));
+		}
+	}
+	
     private static void print(String msg, Object... args) {
         System.out.println(String.format(msg, args));
     }
