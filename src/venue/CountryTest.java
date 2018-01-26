533a805a8752025f7b07b1da40614213642df95b
DamianK2 <dkluziak303@gmail.com>
2018-01-26 21:24:22 +0000

    Added disabled Country unit tests

---
+++ src/venue/CountryTest.java
@@ -0,0 +1,22 @@
+package venue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class CountryTest {
+
+	@Disabled("The method is based of the Locale library")
+	@Test
+	void testCountry() {
+		fail("Not yet implemented");
+	}
+
+	@Disabled("The method is used to retrieve the information that was passed on from the Locale library")
+	@Test
+	void testGetCountries() {
+		fail("Not yet implemented");
+	}
+
+}
