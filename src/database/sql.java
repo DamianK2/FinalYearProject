f691b52590c66fa91302f3ddc018f919973abdc0
DamianK2 <dkluziak303@gmail.com>
2018-03-05 13:54:33 +0000

    Added updating of deadlines in database

--- src/database/sql.java
+++ src/database/sql.java
@@ -56,38 +56,19 @@ public class sql {
 		String query;
 		int venueID = this.addToVenues(venue);
 		
+		// Check if the conference already exists in the database
 		int id = this.checkIfExists(acronym, venueID, currentYear);
+		
 		// If the conference exists then overwrite every value in the database for it (to be changed in the future to check which information needs changing)
 		if(id != -1) {
 			this.updateWebsites(id, acronym, title, sponsors, proceedings, description, venueID, currentYear, antiquity, conferenceDays);
-			this.deleteCommitees(id);
+			this.deleteFromTable(id, "delete from deadlines where id = ?");
+			this.addToDeadlines(id, deadlines);
+			this.deleteFromTable(id, "delete from committees where id = ?");
 			this.addToCommittees(id, organisers);
 		} else {
-			id = this.addToWebsites(acronym, title, sponsors, proceedings, description, venueID, currentYear, antiquity,
-					conferenceDays);
-			
-			query = "insert deadlines (id, deadline_type, deadline_id) values (?, ?, ?)";
-			
-			// Iterate through the map
-			for(String key: deadlines.keySet()) {
-				LinkedHashMap<String, String> actualDeadlines = deadlines.get(key);
-				
-				// The key will either be a string or an integer
-				try {
-					// If the key is an integer, it means there was no type of deadline on the website
-					Integer.parseInt(key);
-					key = "";
-				} catch (NumberFormatException e) {
-					// If it isn't an integer then there is a type present and we don't need to change anything
-				}
-				// This will be the same for one iteration of the map values so only call it once
-				int typeID = this.addToDeadlineTypes(key);
-				
-				// Populate the tables in the database
-				for(String dTitle: actualDeadlines.keySet()) {
-					this.addToTable(query, id, typeID, this.addToDeadlineTitles(dTitle, actualDeadlines.get(dTitle)));
-				}
-			}
+			id = this.addToWebsites(acronym, title, sponsors, proceedings, description, venueID, currentYear, antiquity, conferenceDays);
+			this.addToDeadlines(id, deadlines);
 			this.addToCommittees(id, organisers);
 		}
 	}
@@ -135,23 +116,9 @@ public class sql {
 			int venueID, String currentYear, String antiquity, String conferenceDays) throws SQLException {
 		String updateQuery = "update websites set acronym = ?, title = ?, sponsors = ?, proceedings = ?, description = ?, venueID = ?, current_year = ?, antiquity = ?, conference_days = ? where id = ?";
 		preparedStmt = connection.prepareStatement(updateQuery, Statement.RETURN_GENERATED_KEYS);
-		// Set the variables
-		preparedStmt.setString(1, acronym);
-		preparedStmt.setString(2, title);
-		preparedStmt.setString(3, sponsors);
-		preparedStmt.setString(4, proceedings);
-		preparedStmt.setString(5, description);
-		preparedStmt.setInt(6, venueID);
-		int yearInteger = 0;
-		// Change the year string to an integer
-		try {
-			yearInteger = Integer.parseInt(currentYear);
-		} catch (NumberFormatException e) {
-		}
-
-		preparedStmt.setInt(7, yearInteger);
-		preparedStmt.setString(8, antiquity);
-		preparedStmt.setString(9, conferenceDays);
+		
+		this.setValuesInStatement(acronym, title, sponsors, proceedings, description, venueID, currentYear, antiquity, conferenceDays);
+		// Don't forget to tell which id we are updating
 		preparedStmt.setInt(10, id);
 		
 		this.executeStatement(preparedStmt, true);
@@ -162,12 +129,42 @@ public class sql {
 	 * @param id
 	 * @throws SQLException
 	 */
-	private void deleteCommitees(int id) throws SQLException {
-		String deleteQuery = "delete from committees where id = ?";
+	private void deleteFromTable(int id, String deleteQuery) throws SQLException {
 		preparedStmt = connection.prepareStatement(deleteQuery, Statement.RETURN_GENERATED_KEYS);
 		preparedStmt.setInt(1, id);
 		// Delete everything with the given id from the committees table
 		this.executeStatement(preparedStmt, true);
+	}
+	
+	/**
+	 * Add the given deadlines into the deadlines table
+	 * @param id
+	 * @param deadlines
+	 * @throws SQLException
+	 */
+	private void addToDeadlines(int id, LinkedHashMap<String, LinkedHashMap<String, String>> deadlines) throws SQLException {
+		String query = "insert deadlines (id, deadline_type, deadline_id) values (?, ?, ?)";
+		
+		// Iterate through the map
+		for(String key: deadlines.keySet()) {
+			LinkedHashMap<String, String> actualDeadlines = deadlines.get(key);
+			
+			// The key will either be a string or an integer
+			try {
+				// If the key is an integer, it means there was no type of deadline on the website
+				Integer.parseInt(key);
+				key = "";
+			} catch (NumberFormatException e) {
+				// If it isn't an integer then there is a type present and we don't need to change anything
+			}
+			// This will be the same for one iteration of the map values so only call it once
+			int typeID = this.addToDeadlineTypes(key);
+			
+			// Populate the tables in the database
+			for(String dTitle: actualDeadlines.keySet()) {
+				this.addToTable(query, id, typeID, this.addToDeadlineTitles(dTitle, actualDeadlines.get(dTitle)));
+			}
+		}
 	}
 	
 	/**
@@ -190,29 +187,29 @@ public class sql {
 		}
 	}
 
-	public static void main(String[] args) {
-		try {
-			sql sql = new sql();
-			sql.createConnection();
-			sql.addConference("ICPE", "", "", "", "", "Germany", "2018", "", "", new LinkedHashMap<String, List<String>>(), new LinkedHashMap<String, LinkedHashMap<String, String>>());
-			// PreparedStatement preparedStmt;
-			// String getQuery = "select acronym, committee_name, member from websites,
-			// com_names, member_names, committees where websites.id = 1 and websites.id =
-			// committees.id and committees.titleID = com_names.id and committees.memberID =
-			// member_names.id";
-			// preparedStmt = connection.prepareStatement(getQuery);
-			// ResultSet rs = preparedStmt.executeQuery();
-			//
-			// if(rs.next()) {
-			// System.out.println(rs.getString(1));
-			// System.out.println(rs.getString(2));
-			// System.out.println(rs.getString(3));
-			// }
-		} catch (Exception e) {
-			System.err.println("Got an exception!");
-			System.err.println(e.getMessage());
-		}
-	}
+//	public static void main(String[] args) {
+//		try {
+//			sql sql = new sql();
+//			sql.createConnection();
+//			sql.addConference("ICPE", "", "", "", "", "Germany", "2018", "", "", new LinkedHashMap<String, List<String>>(), new LinkedHashMap<String, LinkedHashMap<String, String>>());
+//			// PreparedStatement preparedStmt;
+//			// String getQuery = "select acronym, committee_name, member from websites,
+//			// com_names, member_names, committees where websites.id = 1 and websites.id =
+//			// committees.id and committees.titleID = com_names.id and committees.memberID =
+//			// member_names.id";
+//			// preparedStmt = connection.prepareStatement(getQuery);
+//			// ResultSet rs = preparedStmt.executeQuery();
+//			//
+//			// if(rs.next()) {
+//			// System.out.println(rs.getString(1));
+//			// System.out.println(rs.getString(2));
+//			// System.out.println(rs.getString(3));
+//			// }
+//		} catch (Exception e) {
+//			System.err.println("Got an exception!");
+//			System.err.println(e.getMessage());
+//		}
+//	}
 
 	/**
 	 * Adds the venue to the venues table if it doesn't exist yet
@@ -247,6 +244,26 @@ public class sql {
 		String mainQuery = "insert websites (acronym, title, sponsors, proceedings, description, venueID, current_year, antiquity, conference_days) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		preparedStmt = connection.prepareStatement(mainQuery, Statement.RETURN_GENERATED_KEYS);
 
+		this.setValuesInStatement(acronym, title, sponsors, proceedings, description, venueID, currentYear, antiquity, conferenceDays);
+		
+		return this.executeStatement(preparedStmt, false);
+	}
+	
+	/**
+	 * Set the selected values for the statement to be executed in the websites table
+	 * @param acronym
+	 * @param title
+	 * @param sponsors
+	 * @param proceedings
+	 * @param description
+	 * @param venueID
+	 * @param currentYear
+	 * @param antiquity
+	 * @param conferenceDays
+	 * @throws SQLException
+	 */
+	private void setValuesInStatement(String acronym, String title, String sponsors, String proceedings, String description,
+			int venueID, String currentYear, String antiquity, String conferenceDays) throws SQLException {
 		// Set the variables
 		preparedStmt.setString(1, acronym);
 		preparedStmt.setString(2, title);
@@ -264,10 +281,15 @@ public class sql {
 		preparedStmt.setInt(7, yearInteger);
 		preparedStmt.setString(8, antiquity);
 		preparedStmt.setString(9, conferenceDays);
-		
-		return this.executeStatement(preparedStmt, false);
 	}
 
+	/**
+	 * Add the type of deadline to the deadline_types table
+	 * 
+	 * @param type
+	 * @return id of changed row
+	 * @throws SQLException
+	 */
 	private int addToDeadlineTypes(String type) throws SQLException {
 		// Check to lower case to avoid duplicates with capital letters etc.
 		String searchType = "select id from deadline_types where lower(d_type) = \"" + type.toLowerCase() + "\"";
@@ -275,6 +297,14 @@ public class sql {
 		return this.checkAndReturn(searchType, deadlineType, type);
 	}
 	
+	/**
+	 * Add the title and date to the deadlines_titles table
+	 * 
+	 * @param title
+	 * @param date
+	 * @return id of changed row
+	 * @throws SQLException
+	 */
 	private int addToDeadlineTitles(String title, String date) throws SQLException {
 		String deadlineTitle = "insert deadlines_titles (d_title, d_date) values (?, ?)";
 		preparedStmt = connection.prepareStatement(deadlineTitle, Statement.RETURN_GENERATED_KEYS);
@@ -352,7 +382,7 @@ public class sql {
 		if (rs.next()) {
 			// Find the id from the set
 			id = rs.getInt(1);
-			System.out.println("(checkAndReturn)found item id: " + id);
+//			System.out.println("(checkAndReturn)found item id: " + id);
 			return id;
 		} else {
 			preparedStmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
