5367908b4fbd6f0430c335bd957a9e6a5a756cc1
DamianK2 <dkluziak303@gmail.com>
2018-03-05 14:16:32 +0000

    Added country tests

--- src/venue/CountryTest.java
+++ src/venue/CountryTest.java
@@ -1,22 +1,27 @@
 package venue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class CountryTest {
 
-	@Disabled("The method is based of the Locale library")
 	@Test
 	void testCountry() {
-		
+		Country country = new Country();
+		ArrayList<String> countries = country.getCountries();
+		assertTrue(countries.contains("USA"));
+		assertTrue(countries.contains("Catalonia"));
+		assertTrue(countries.contains("Goias"));
+		assertTrue(countries.contains("Sri Lanka"));
 	}
 
 	@Disabled("The method is used to retrieve the information that was passed on from the Locale library")
 	@Test
 	void testGetCountries() {
-		
 	}
 
 }
